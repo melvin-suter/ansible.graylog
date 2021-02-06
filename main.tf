c0c25c3fe373c77fa2ada28c687a97c26f0dca72
Melvin Suter <melvin@suter.dev>
2021-02-06 12:12:29 +0100

    add terraform file

---
+++ main.tf
@@ -0,0 +1,42 @@
+# Startup Terraform
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  owners           = ["aws-marketplace"]
+
+  filter {
+    name   = "name"
+    values = ["Centos 8 - basic installation"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Create a EC2 Instance
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos8.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
